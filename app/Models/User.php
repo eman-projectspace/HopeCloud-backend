2448d4f67894e42c96a21515108025d738d79dd6
eman-projectspace <emanwaheed001@gmail.com>
2026-08-31 16:52:04 +0500

    added sactum

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,18 @@
+```php
 <?php
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -47,3 +48,5 @@ class User extends Authenticatable
         ];
     }
 }
+```
+
